86e66edaa4ebaaefaf789b7b10d394c528034752
Bart Masters <bmasters@homeaway.com>
2020-01-31 14:56:01 +1100

    WIP

---
+++ fcc/IntermediateAlgorithms/addTogether.js
@@ -0,0 +1,5 @@
+function addTogether() {
+  return false;
+}
+
+addTogether(2, 3);
---
+++ fcc/IntermediateAlgorithms/makeAPerson.js
@@ -0,0 +1,10 @@
+var Person = function(firstAndLast) {
+  // Complete the method below and implement the others similarly
+  this.getFullName = function() {
+    return '';
+  };
+  return firstAndLast;
+};
+
+var bob = new Person('Bob Ross');
+bob.getFullName();
