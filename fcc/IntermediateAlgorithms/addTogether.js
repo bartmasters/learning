b03e0f00abe0d0711dc187f1358193d130e72d42
Bart Masters <bmasters@homeaway.com>
2020-02-01 15:02:33 +1100

    WIP

--- fcc/IntermediateAlgorithms/addTogether.js
+++ fcc/IntermediateAlgorithms/addTogether.js
@@ -1,5 +1,25 @@
 function addTogether() {
-  return false;
-}
+  if (arguments.length > 2) {
+    return undefined;
+  }
+  if (arguments.length == 2) {
+    if (typeof arguments[0] !== 'number' || typeof arguments[1] !== 'number') {
+      return undefined;
+    }
+    return arguments[0] + arguments[1];
+  }
 
-addTogether(2, 3);
+  let myVar = arguments[0];
+  if (typeof myVar !== 'number') {
+    return undefined;
+  }
+
+  return function(newNum) {
+    if (typeof newNum != 'number') {
+      return undefined;
+    }
+    return myVar + newNum;
+  };
+}
+let bob = addTogether(3)(5);
+console.log(bob);
