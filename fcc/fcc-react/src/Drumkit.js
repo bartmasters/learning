f831d509afac34155bfbc32106da35e245320c86
Bart Masters <bmasters@homeaway.com>
2020-02-06 22:07:51 +1100

    WIP

--- fcc/fcc-react/src/Drumkit.js
+++ fcc/fcc-react/src/Drumkit.js
@@ -3,44 +3,54 @@ import React from 'react';
 const keys = [
   {
     letter: 'Q',
+    id: 'Heater-1',
     clip: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
   },
   {
     letter: 'W',
+    id: 'Heater-2',
     clip: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
   },
   {
     letter: 'E',
+    id: 'Heater-3',
     clip: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
   },
   {
     letter: 'A',
+    id: 'Heater-4_1',
     clip: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
   },
   {
     letter: 'S',
+    id: 'Heater-6',
     clip: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
   },
   {
     letter: 'D',
+    id: 'Dsc_Oh',
     clip: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
   },
   {
     letter: 'Z',
+    id: 'Kick_n_Hat',
     clip: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
   },
   {
     letter: 'X',
+    id: 'RP4_KICK_1',
     clip: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
   },
   {
     letter: 'C',
+    id: 'Cev_H2',
     clip: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
   }
 ];
 
 const Pad = props => (
-  <div class="drum-pad" id={props.clip}>
+  <div class="drum-pad" id={props.id}>
+    <audio src={props.clip} type="audio/mpeg" class="clip" id={props.letter} />
     {props.letter}
   </div>
 );
@@ -60,7 +70,7 @@ class Drumkit extends React.Component {
         <h1>Bart's Drumkit</h1>
         <div class="grid">
           {keys.map(key => (
-            <Pad props={key} />
+            <Pad id={key.id} letter={key.letter} clip={key.clip} />
           ))}
         </div>
         <div id="display">This is the display</div>
