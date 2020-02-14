ec5e908e8b2eab4f7148e8edcaa01ca98ae28eeb
Bart Masters <bart.masters@gmail.com>
2020-02-14 15:16:28 +1100

    Set up Pomodoro

--- fcc/fcc-react/src/index.js
+++ fcc/fcc-react/src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 //import Markdown from './Markdown';
 //import Drumkit2 from './Drumkit2';
-import Calculator from './Calculator';
+//import Calculator from './Calculator';
+import Pomodoro from './Pomodoro';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Calculator />, document.getElementById('root'));
+ReactDOM.render(<Pomodoro />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
