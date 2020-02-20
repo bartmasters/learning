5c24c6d31fed3b560095d3ca89349538472974b9
Bart Masters <bart.masters@gmail.com>
2020-02-20 22:22:18 +1100

    WIP

--- fcc/fcc-react/src/Pomodoro.js
+++ fcc/fcc-react/src/Pomodoro.js
@@ -6,19 +6,27 @@ const defaultBreak = 300;
 function convertToMins(origSecs) {
   let mins = Math.floor(origSecs / 60);
   let secs = origSecs - mins * 60;
-  return mins + ':' + secs;
+  return (mins < 10 ? '0' : '') + mins + ':' + (secs < 10 ? '0' : '') + secs;
 }
 function Pomodoro() {
   const [timeLeft, setTimeLeft] = useState(defaultSession);
   const [runningState, setRunningState] = useState('stop');
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setTimeLeft(timeLeft => timeLeft - 1);
-    }, 1000);
-    return () => {
-      clearInterval(interval);
-    };
+    if (runningState === 'run') {
+      const interval = setInterval(() => {
+        if (timeLeft >= 1) {
+          setTimeLeft(timeLeft - 1);
+        } else {
+          console.log('Times up');
+          setRunningState('stop');
+          clearInterval(interval);
+        }
+      }, 1000);
+      return () => {
+        clearInterval(interval);
+      };
+    }
   });
 
   const handleRunningState = () => {
@@ -31,31 +39,31 @@ function Pomodoro() {
 
   return (
     <div>
-      <div className='outside-box' id='calculator'>
+      <div className="outside-box" id="calculator">
         <h1>Bart's Pomodoro</h1>
-        <div className='grid'>
-          <div id='break'>
-            <h2 id='break-label'>Break Length</h2>
+        <div className="grid">
+          <div id="break">
+            <h2 id="break-label">Break Length</h2>
           </div>
-          <div id='session'>
-            <h2 id='session-label'>Session Length</h2>
+          <div id="session">
+            <h2 id="session-label">Session Length</h2>
           </div>
-          <button
-            className='controlpad'
-            id='start_stop'
+          <input
+            type="image"
+            src="play-pause-svgrepo-com.svg"
+            alt="Play/pause"
+            id="start_stop"
+            width="48"
+            height="48"
             onClick={() => {
               handleRunningState();
             }}
-          >
-            {'Start/Stop'}
-          </button>
+          />
         </div>
-        <div id='time-left'>{convertToMins(timeLeft)}</div>
+        <div id="time-left">{convertToMins(timeLeft)}</div>
       </div>
     </div>
   );
 }
 
 export default Pomodoro;
-
-// https://medium.com/@kristin_baumann/react-countdown-6455838b6faf
