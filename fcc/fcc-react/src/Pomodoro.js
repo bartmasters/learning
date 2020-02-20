6cc96ea3d0c92101936ea9b6142c2590d1c86899
Bart Masters <bmasters@homeaway.com>
2020-02-20 20:14:45 +1100

    WIP - Include svg icons

--- fcc/fcc-react/src/Pomodoro.js
+++ fcc/fcc-react/src/Pomodoro.js
@@ -1,34 +1,56 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
+const defaultSession = 15; //1500
+const defaultBreak = 300;
+
+function convertToMins(origSecs) {
+  let mins = Math.floor(origSecs / 60);
+  let secs = origSecs - mins * 60;
+  return mins + ':' + secs;
+}
 function Pomodoro() {
-  const [timeLeft, setTimeLeft] = useState('60');
-  const [startStop, setStartStop] = useState('stop');
+  const [timeLeft, setTimeLeft] = useState(defaultSession);
+  const [runningState, setRunningState] = useState('stop');
 
-  const handleStartStop = () => {
-    if (startStop === 'start') {
-      setStartStop('stop');
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimeLeft(timeLeft => timeLeft - 1);
+    }, 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  });
+
+  const handleRunningState = () => {
+    if (runningState === 'run') {
+      setRunningState('stop');
     } else {
-      setStartStop('start');
+      setRunningState('run');
     }
-    //setisplay('0');
   };
 
   return (
     <div>
-      <h1>Bart's Pomodoro</h1>
-      <div className="outside-box" id="calculator">
-        <div className="grid">
+      <div className='outside-box' id='calculator'>
+        <h1>Bart's Pomodoro</h1>
+        <div className='grid'>
+          <div id='break'>
+            <h2 id='break-label'>Break Length</h2>
+          </div>
+          <div id='session'>
+            <h2 id='session-label'>Session Length</h2>
+          </div>
           <button
-            className="controlpad"
-            id="start_stop"
+            className='controlpad'
+            id='start_stop'
             onClick={() => {
-              handleStartStop();
+              handleRunningState();
             }}
           >
             {'Start/Stop'}
           </button>
         </div>
-        <div id="time-left">{timeLeft}</div>
+        <div id='time-left'>{convertToMins(timeLeft)}</div>
       </div>
     </div>
   );
